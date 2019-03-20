21a983ff59a89719dc1e3e92381f65564623f8b5
Maniccia <mariamaniccia@gmail.com>
2019-03-19 21:32:25 -0400

    deployment

--- my-app/models/books.js
+++ my-app/models/books.js
@@ -6,33 +6,29 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 // Create the Book schema
-// title - Title of the book from the Google Books API
-// authors - The books's author(s) as returned from the Google Books API
-// description - The book's description as returned from the Google Books API
-// image - The Book's thumbnail image as returned from the Google Books API
-// link - The Book's information link as returned from the Google Books API
 const BookSchema = new Schema({
   body: {
     title: {
-        type: String,
-        required: true,
-      },
-      authors: {
-        type: String,
-        required: true,
-      },
-      description: {
-        type: String,
-        required: true,
-      },
-      image: {
-        type: String,
-        required: true,
-      },
-      link: {
-        type: String,
-        required: true,
-      },
+      type: String,
+      required: true,
+    },
+    authors: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+    },
+  },
 });
 
 // Create the model
