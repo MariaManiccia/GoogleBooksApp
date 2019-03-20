35fa82158fb4af4cd6f6cd0bc084421702b9f60c
Maniccia <mariamaniccia@gmail.com>
2019-03-19 21:31:28 -0400

    Updates

---
+++ precache-manifest.3698339f40d98cbb2e6833f2d872c5f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "57d15df86b25bdf20924",
+    "url": "/static/css/main.24e1fd5a.chunk.css"
+  },
+  {
+    "revision": "57d15df86b25bdf20924",
+    "url": "/static/js/main.ec4dbe8f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "58cc2a2f34a3f8e19d08cee38635b63a",
+    "url": "/index.html"
+  }
+];
